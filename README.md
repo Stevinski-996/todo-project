b74764dfc0877f2f44411e278c28d3d246c80ef8
Steve Hinity <162119747+Stevinski-996@users.noreply.github.com>
2024-04-29 22:07:59 +0300

    Todo js2 (#3)
    * Lab 6 Continuation
    
    * Lab 6 Updates
    
    * adding app.js to the project
    
    * Update on lab 05
    
    * Update on Lab 05

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # todo-project
 ![Wire Frame Design](Images/Wireframe.jpg)
+## Lab 5 
+a new js code has been added to the project to make it more interactive which introduced 
+** Popup boxes 
+** Declaration
+** if statement 
+## Lab 6 
+update has been uploaded to the same js code so it can include and introduce new topics : 
+** Loops (for,while)
+** Declaration Scopes 
+** Functions 
+** Arrays
--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 'use strict' ; 
 
-var userName = prompt("Enter your Name...")
-var userGender = prompt("Enter Your Gender (male/female)...")
-var userAge = prompt("Enter Your Age ...");
+let userName = prompt("Enter your Name...")
+let userGender = prompt("Enter Your Gender (male/female)...")
+let userAge = prompt("Enter Your Age ...");
+let anss=[]
+function storingAns () {
+    anss.push(userName) ;
+    anss.push(userGender);
+    anss.push(userAge);
+    return anss ;
+}
+
 if (userAge<=0) {
     alert("Age is not valid!");
 }
@@ -15,3 +23,34 @@ else {
     alert("Hello!")
 }
 }
+function answerValidation(question) {
+    let answer = prompt(question + "(yes/no)").toLowerCase(); 
+  
+    while (answer !== "yes" && answer !== "no") { 
+      alert("Invalid answer please answer with 'Yes' or 'No'");
+      answer = prompt(question + "(Yes/No)").toLowerCase(); 
+    }
+    return answer;
+  }
+  
+  function askQuestion(question) {
+    let answer = answerValidation(question);
+    return answer;
+  }
+  
+  function storeAnswers() { 
+    let answerArray = [];
+    const questions = ["Do you enjoy learning new things?", "Is staying organized important to you?", "Are you a good team player?"];
+    for (let i = 0; i < questions.length; i++) {
+      let answer = askQuestion(questions[i]);
+      answerArray.push(answer);
+    }
+  
+    return answerArray;
+  }
+  
+  let allAnswers = storeAnswers();
+  let firstAnswers = storingAns();
+
+  console.log("2nd Answers="+allAnswers);
+  console.log("1st Answer="+firstAnswers);
